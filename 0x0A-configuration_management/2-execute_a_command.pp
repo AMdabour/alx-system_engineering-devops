7a088c5c6053e946ad115f553bb8c02c00d8ed73
a-dabour <ah910905@gmail.com>
2023-09-22 11:50:48 -0700

    edit

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # puppet manifest to kill a process called 'killmenow'
 exec { 'kill_killmenow':
-  command     => 'pkill -9 -f killmenow',
-  path        => ['/bin', '/usr/bin', '/usr/sbin', '/sbin'],
+  path    => '/bin/',
+  command => 'pkill killmenow',
 }
