4b914f4845ec9ad990426ea7c3466ed8a07ed2fd
a-dabour <ah910905@gmail.com>
2023-09-22 20:23:11 +0300

    initial commit creating a file in /tmp directory

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# this puppet manifest creates a file called school in /tmp dir
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
