07940d72a36627f31466bbda0df5f84ce05c4075
a-dabour <ah910905@gmail.com>
2023-09-23 16:15:13 +0300

    editing ssh_config file with puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# setting up my ssh client config file
+
+file_line { 'disabling password authentication':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delaring identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
+}
