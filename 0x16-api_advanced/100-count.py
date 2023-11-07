3bbae1b05bf495bc0f1447d6077a2e6f5c07f965
a-dabour <ah910905@gmail.com>
2023-11-07 18:26:11 +0200

    updating no match posts

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -38,7 +38,6 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
 
     if after is None:
         if len(instances) == 0:
-            print("")
             return
         instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
         [print("{}: {}".format(k, v)) for k, v in instances]
